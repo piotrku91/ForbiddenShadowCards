83490d867532d64852dfd3ec433e28d47c8f422d
Piotr <piotrku91@github.com>
2021-07-22 17:12:39 +0200

    first commit / sketch of Card classes ;

---
+++ Card.cpp
@@ -0,0 +1,46 @@
+#include <string>
+
+class CharacterCard
+{
+private:
+std::string Name_;
+size_t Lifepoints_;
+
+};
+
+class Card
+{
+std::string Name_;
+
+
+};
+
+class BattleCard : public Card
+{
+
+
+
+};
+
+class DefenseCard : public Card
+{
+
+
+
+};
+
+class ModifierCard : public Card
+{
+
+
+
+};
+
+class SpecialCard : public Card
+{
+
+
+
+};
+
+
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Card.cpp"
+
+int main() 
+{
+std::cout << "Start" << std::endl;
+
+}
