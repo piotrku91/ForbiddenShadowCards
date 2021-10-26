6b80db04d109ea1c87ccef4fab09b87b80ad6737
Piotr <piotrku91@github.com>
2021-10-26 16:19:53 +0200

    new idea

--- main.cpp
+++ main.cpp
@@ -1,56 +1,10 @@
 #include <iostream>
 #include "CardsActionManager.hpp"
-#include "simplerect.hpp"
-#include "window.hpp"
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_timer.h>
 
 CardsActionManager CAM;
 
 int main() {
-    // SDL Initialization
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        printf("error initializing SDL: %s\n", SDL_GetError());
-    }
-    // Main window object creation
-    RenderWindow mainWindow{"Battle Card Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1366, 768};
-
-    std::vector<std::shared_ptr<simplerect>> battleTable;
-    battleTable.push_back(std::make_shared<simplerect>(simplerect{mainWindow.getSizeX() - 400 - 20, 20, 400, 100, 0, 0, 50, 255, mainWindow.getRenderer()}));
-    battleTable.push_back(std::make_shared<simplerect>(simplerect{20, mainWindow.getSizeY() - 150 - 20, 400, 100, 0, 0, 50, 255, mainWindow.getRenderer()}));
-    battleTable.push_back(std::make_shared<simplerect>(spriterect{20, 0, 20, 20, mainWindow.getRenderer()}));
-
-    //Event section
-    bool close = false;
-    while (!close) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-            case SDL_QUIT:
-                // handling of close button
-                close = true;
-                break;
-            };
-        };
-
-        //Render section
-        SDL_RenderClear(mainWindow.getRenderer());
-        SDL_RenderCopy(mainWindow.getRenderer(), mainWindow.getBackgroundTexture(), NULL, mainWindow.getBackgroundRect());
-
-        // Render rect
-        for (auto& rect : battleTable) {
-            rect->renderAndDraw();
-        };
-
-        SDL_RenderPresent(mainWindow.getRenderer());
-        SDL_Delay(1000 / 60);
-    };
-
-    // Clean section
-    mainWindow.Destroy();
-    SDL_Quit();
 
     return 0;
 }
--- window.hpp
+++ window.hpp
@@ -1,62 +1,53 @@
 #pragma once
 
+#include <iostream>
 #include <string>
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
 
-class RenderWindow final {
+class Window final {
 private:
     SDL_Window* window_;
     SDL_Renderer* renderer_;
-    SDL_Texture* background_;
-    SDL_Rect backg_rect_;
     bool isDestroyed;
 
 public:
-    RenderWindow(const std::string& windowCaption, int posX, int posY, int sizeW, int sizeH, Uint32 flags = SDL_WINDOW_SHOWN)
-        : backg_rect_{0, 0, sizeW, sizeH}, isDestroyed(false) {
-        window_ = SDL_CreateWindow(windowCaption.c_str(), posX, posY, sizeW, sizeH, flags);
+    Window(const std::string& Caption, int sizeW, int sizeH)
+        : isDestroyed(false) {
+        window_ = SDL_CreateWindow(Caption.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, sizeW, sizeH, SDL_WINDOW_SHOWN);
+        if (window_ == NULL) {
+            std::cout << "Window Error: " << SDL_GetError() << std::endl;
+            throw;
+        }
         renderer_ = SDL_CreateRenderer(window_, -1, SDL_RENDERER_ACCELERATED);
-
-        SDL_Surface* surface =IMG_Load("textures/bg.jpeg");
-        background_ = SDL_CreateTextureFromSurface(renderer_, surface);
-        SDL_FreeSurface(surface);
-        SDL_QueryTexture(background_, NULL, NULL, &backg_rect_.w, &backg_rect_.h);
     };
 
-    ~RenderWindow() {
+    ~Window() {
         if (!isDestroyed) {
-            Destroy();
+            destroy();
         };
     }
 
-    void Destroy() {
-        SDL_DestroyTexture(background_);
-        SDL_DestroyRenderer(renderer_);
+    void render(int posX, int posY, SDL_Texture* texture) {
+        SDL_Rect source_rect;
+        source_rect.x = 0;
+        source_rect.y = 0;
+        SDL_QueryTexture(texture, NULL, NULL, &source_rect.w, &source_rect.h);
+        SDL_Rect dest_rect{posX, posY, source_rect.w, source_rect.h};
+        SDL_RenderCopy(renderer_, texture, &source_rect, &dest_rect);
+    }
+
+    void destroy() {
         SDL_DestroyWindow(window_);
         isDestroyed = true;
     }
 
-    void getSize(int* tmpX, int* tmpY) {
-        SDL_GetWindowSize(window_, tmpX, tmpY);
-    }
-
-    int getSizeX() {
-        int tmpX, tmpY;
-        SDL_GetWindowSize(window_, &tmpX, &tmpY);
-        return tmpX;
-    }
-
-    int getSizeY() {
-        int tmpX, tmpY;
-        SDL_GetWindowSize(window_, &tmpX, &tmpY);
-        return tmpY;
+    void clear() {
+        SDL_RenderClear(renderer_);
     }
 
     SDL_Renderer* getRenderer() { return renderer_; };
     SDL_Window* getWindow() const { return window_; };
-    SDL_Texture* getBackgroundTexture() const { return background_; };
-    SDL_Rect* getBackgroundRect() { return &backg_rect_; };
 };
